9b119b256252e9c5e2ad0099459d291e6df69a98
xxMVG_Fan8xx <xxmvgfan8xx@yahoo.com>
2025-07-24 17:09:08 +0200

    Added index.js

---
+++ index.js
@@ -0,0 +1,85 @@
+const discord = require("discord.js");
+const client = new discord.Client({ intents: ["GuildMessages", "GuildMembers", "Guilds", "MessageContent"] });
+const config = require("./config.json");
+const token = process.env.TOKEN;
+const prefix = config.prefix;
+
+client.login(token)
+
+
+client.on("ready", async readyclient => {
+    console.log(`${readyclient.user.tag} is ready`)
+})
+
+function isCommand(command, message) {
+    let msg = message.content.toLowerCase();
+    let cmd = command.toLowerCase()
+    return msg.startsWith(prefix + cmd)
+}
+
+
+
+function sendError(err, msg) {
+    let embed = createErrorEmbed(err);
+    msg.reply({ embeds: [embed] });
+}
+
+function sendWrongSyntax(err, syntax, msg) {
+    let embed = new discord.EmbedBuilder();
+    embed.setTitle("WRONG/MISSING SYNTAX")
+    embed.addFields(
+        {
+            name: 'Error',
+            value: err.toString(),
+            inline: false
+        },
+        {
+            name: "Format",
+            value: `\`${syntax}\``,
+            inline: false
+        }
+    );
+
+    embed.setColor("Red");
+    msg.reply({ embeds: [embed] });
+}
+
+function sendInsufficientPermission(current, msg) {
+    let embed = new discord.EmbedBuilder();
+    embed.setTitle("Insufficient Permission")
+        .setDescription("You are not permitted to run this command.")
+    embed.addFields(
+        {
+            name: 'Required Rank',
+            value: current.toString(),
+            inline: true
+        }
+    );
+
+    embed.setColor("Red");
+    msg.reply({ embeds: [embed] });
+}
+
+
+client.on('messageCreate', async message => {
+    checkLoas();
+    if (message.author.bot) return;
+    const args = message.content.split(' ');
+
+    if (isCommand("test", message)) {
+        message.reply("Test successful")
+    }
+    if(isCommand("exit",message)) {
+        if(message.member.id == "424895323660484610"){
+            process.exit(1);
+        }
+    }
+});
+
+client.on("guildMemberAdd",async member => {
+    member.roles.add(await member.guild.roles.fetch("997185740276510720"));
+})
+
+process.on('exit', async (code) => {
+    await client.destroy();
+  });
