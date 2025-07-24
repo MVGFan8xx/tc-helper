9e1d1f5082d2cba619329d34149b5d35754511ce
xxMVG_Fan8xx <xxmvgfan8xx@yahoo.com>
2025-07-24 17:22:12 +0200

    Added app

--- index.js
+++ index.js
@@ -60,6 +60,17 @@ function sendInsufficientPermission(current, msg) {
     msg.reply({ embeds: [embed] });
 }
 
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 
 client.on('messageCreate', async message => {
     checkLoas();
